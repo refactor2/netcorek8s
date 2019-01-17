c74ea30fe6ed8cdb55110cab6881d134cbc32789
refactor <295240648@163.com>
2019-01-17 11:22:10 +0800

    init

---
+++ netcorek8s/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netcorek8s.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo("hostname");
+            psi.RedirectStandardOutput = true;
+            using (var process = System.Diagnostics.Process.Start(psi))
+            {
+                var computerName = process.StandardOutput.ReadToEnd();
+                return Content(computerName);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
