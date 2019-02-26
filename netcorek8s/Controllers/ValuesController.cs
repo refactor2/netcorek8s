6fb28c0bacfd222a05775ae9dfeeba9db29d74b6
refactor <295240648@163.com>
2019-02-26 13:37:33 +0800

    添加version标识

--- netcorek8s/Controllers/ValuesController.cs
+++ netcorek8s/Controllers/ValuesController.cs
@@ -14,14 +14,7 @@ namespace netcorek8s.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return Content(System.Environment.MachineName);
-            var psi = new System.Diagnostics.ProcessStartInfo("hostname");
-            psi.RedirectStandardOutput = true;
-            using (var process = System.Diagnostics.Process.Start(psi))
-            {
-                var computerName = process.StandardOutput.ReadToEnd();
-                return Content(computerName);
-            }
+            return Content("v2 from "+System.Environment.MachineName);
         }
 
         // GET api/values/5
